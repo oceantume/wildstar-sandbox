2b2e018d9bd96758bd522b390c6ddd9353a42521
Oceantume <gabriel.bourgeoisv4si@gmail.com>
2022-10-13 19:27:11 -0400

    Add some tests for message structure and union macros

--- crates/ws_messages/src/macros.rs
+++ crates/ws_messages/src/macros.rs
@@ -4,11 +4,121 @@ pub use ws_messages_macros::*;
 mod tests {
     use crate as ws_messages;
     use ws_bitpack::*;
-    use ws_messages::*;
     use ws_messages::reader::*;
     use ws_messages::writer::*;
+    use ws_messages::*;
 
     use std::io::{Cursor, Read};
+
+    fn write_and_read<T>(input: &T) -> T
+    where
+        MessageWriter<T>: SimpleWriter<T>,
+        MessageReader<T>: SimpleReader<T>,
+    {
+        let mut cursor = Cursor::new(Vec::new());
+        let mut writer = BitPackWriter::new(&mut cursor);
+        MessageWriter::write(&mut writer, input).unwrap();
+        writer.align_and_flush().unwrap();
+        cursor.set_position(0);
+        let mut reader = BitPackReader::new(&mut cursor);
+        MessageReader::read(&mut reader).unwrap()
+    }
+
+    #[test]
+    fn test_vec_write_read() {
+        #[derive(MessageStruct)]
+        struct Struct {
+            count: u32,
+            #[length(count)]
+            items: Vec<u32>,
+        }
+        let in_value = Struct {
+            count: 5,
+            items: vec![1, 2, 3, 4, 5],
+        };
+        let out_value = write_and_read(&in_value);
+        assert_eq!(in_value.count, out_value.count);
+        assert_eq!(in_value.items, out_value.items);
+    }
+
+    #[test]
+    fn test_packed_write_read() {
+        #[derive(MessageStruct)]
+        struct Struct {
+            #[packed(5)]
+            value: u32,
+        }
+        let in_value = Struct { value: 5 };
+        let out_value = write_and_read(&in_value);
+        assert_eq!(in_value.value, out_value.value);
+    }
+
+    #[test]
+    fn test_packed_vec_write_read() {
+        #[derive(MessageStruct)]
+        struct Struct {
+            count: u32,
+            #[packed(5)]
+            #[length(count)]
+            items: Vec<u32>,
+        }
+        let in_value = Struct {
+            count: 5,
+            items: vec![1, 2, 3, 4, 5],
+        };
+        let out_value = write_and_read(&in_value);
+        assert_eq!(in_value.count, out_value.count);
+        assert_eq!(in_value.items, out_value.items);
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid union variant 2")]
+    fn test_union() {
+        #[derive(MessageUnion)]
+        enum Union {
+            Unsigned64 { value: u64 },
+            Signed16 { value: i16 },
+        }
+        #[derive(MessageStruct)]
+        struct Struct {
+            id: u32,
+            #[variant(id)]
+            union: Union,
+        }
+
+        // test first variant
+        let in_value = Struct {
+            id: 0,
+            union: Union::Unsigned64 {
+                value: 123456789123456789,
+            },
+        };
+        let out_value = write_and_read(&in_value);
+        let out_union_value = match out_value.union {
+            Union::Unsigned64 { value } => Some(value),
+            _ => None,
+        };
+
+        // test second variant
+        assert_eq!(out_union_value, Some(123456789123456789));
+        let in_value = Struct {
+            id: 1,
+            union: Union::Signed16 { value: -12349 },
+        };
+        let out_value = write_and_read(&in_value);
+        let out_union_value = match out_value.union {
+            Union::Signed16 { value } => Some(value),
+            _ => None,
+        };
+        assert_eq!(out_union_value, Some(-12349));
+
+        // test invalid variant (should panic during read with above message)
+        let in_value = Struct {
+            id: 2,
+            union: Union::Signed16 { value: 0 },
+        };
+        write_and_read(&in_value);
+    }
 
     #[derive(MessageStruct)]
     struct Message0002 {
